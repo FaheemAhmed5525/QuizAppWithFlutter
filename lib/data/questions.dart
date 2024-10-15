d49b266dfe3bab69eadac19d6a6bb395575ff603
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-10-15 15:42:33 +0500

    data added

---
+++ lib/data/questions.dart
@@ -0,0 +1,35 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+
+const questions = [
+  QuizQuestion(
+    "What are the main building blocks of Flutters UI's", 
+    [
+      'Widgets',
+      "Components",
+      "Blocks",
+      "Functions",
+    ]
+  ),
+  QuizQuestion(
+    "How are Flutter UIs built?", 
+    [
+      "By combining widgets in code",
+      "By combining widgets in a visual editor",
+      "By defining widgets ina config files",
+      "By using XCode for iOS, and Android Studio for Android",
+    ]
+  ),
+  QuizQuestion(
+    "What is the purpose of a StatefulWidget?", 
+    [
+      "Update UI as data changes",
+      "Update data as UI changes"
+      "Ignore data changes",
+      "Render UI that does not depend on data",
+    ]
+  ),
+  
+  
+
+];
