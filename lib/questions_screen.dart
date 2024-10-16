f43eebc27e7af9e3c3e82705ffe06c23de0b3757
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-10-16 09:27:41 +0500

    question loading machanism completed

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
-import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,36 +12,40 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex += 1;
+    });
+  }
+
   @override
   Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Question is ...",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w500)),
-          SizedBox(height: 32),
-          AnswerButton(
-            answerText: "answer text1", 
-            onTap: () {}
-          ),
-          AnswerButton(
-            answerText: "answer text", 
-            onTap: () {}
-          ),
-          AnswerButton(
-            answerText: "answer text3", 
-            onTap: () {}
-          ),
-          AnswerButton(
-            answerText: "answer text4", 
-            onTap: () {}
-          )
-        ],
+      child: Container(
+        margin: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(currentQuestion.text,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500
+                ),
+                textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(answerText: item, onTap: answerQuestion);
+            })
+          ],
+        ),
       ),
     );
   }
